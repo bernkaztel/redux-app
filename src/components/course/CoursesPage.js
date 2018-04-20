6373cfd5be1272d964f36d715f80fee47aaaf5c3
bernkaztel <bernkaztel@hotmail.com>
2018-04-19 21:01:03 -0500

    quitando componentes de renderizado en CoursesPage

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -8,24 +8,28 @@ class CoursesPage extends Component {
     this.onTitleChange = this.onTitleChange.bind(this);
     this.onClickSave = this.onClickSave.bind(this);
     //Declaramos un estado para almacenar el valor del input
-    this.state = {
-      course: { title: "" }
-    }
+    // this.state = {
+    //   course: { title: "" }
+    // }
   }
+
+
   //Cada vez que el input cambie, el valor se va a almacenar en el estado. 
-  onTitleChange(event){
+  // onTitleChange(event){
     //Sacamos una copia del estado. 
-    const courseCopy = this.state.course;
+    // const courseCopy = this.state.course;
     //Modificamos la copia
-    courseCopy.title = event.target.value
+    // courseCopy.title = event.target.value
     //Enviamos de vuelta el estado 
-    this.setState({ course: courseCopy });
-  }
+  //   this.setState({ course: courseCopy });
+  // }
+
+
   //Cuando se le da el botón de click, se manda la action y ésta se envía al reducer con el dispatch
-  onClickSave(){
+  // onClickSave(){
     //Se envía la acción por medio del creador de acciones envíandole el curso(contenido en el estado)
-    this.props.createCourse(this.state.course);
-  }
+  //   this.props.createCourse(this.state.course);
+  // }
   //Función para pintar los cursos
   courseRow(course, index){
     return <div key={index}>{course.title}</div>;
@@ -35,16 +39,6 @@ class CoursesPage extends Component {
       <div className="coursespage">
         <h1>Courses</h1>
         {this.props.coursesFromReducer.map(this.courseRow)}
-        <input
-          type="text" 
-          onChange={this.onTitleChange}
-          ref="title-input"
-        />
-          <input
-          type="submit"
-          value="save"
-          onClick={this.onClickSave}
-        />
       </div>
     );
   }
